b39db11036c51079bdf934ea1c9b944ec87f7b1e
vadimchik-hohlorez <166957508+vadimchik-hohlorez@users.noreply.github.com>
2024-04-14 04:32:35 +0300

    Update main.py

--- rest_api/main.py
+++ rest_api/main.py
@@ -1,11 +1,19 @@
 import base64
-from typing import BinaryIO
-
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import logging
 from pydantic import BaseModel
 from fastapi_storages import FileSystemStorage
+import easyocr
+import sys
+sys.path.append('OCR-product-label')
+import io
+from PIL import Image
+from models.utils import preprocessing_image
+
+
+# download weights model
+reader = easyocr.Reader(['ru'])
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
@@ -20,19 +28,22 @@ class Data(BaseModel):
 @app.post("/analyze-image")
 async def analyze_image(data: Data):
 
-    # Сохраняем изображение на сервере
-    binf = BinaryIO()
-    binf.write(data.file.encode('ascii'))
-    storage.write(binf, name=str(hash(data.file)))
+    def save_base64_to_png(base64_string, output_path):
+        image_data = base64.b64decode(base64_string)
+        image = Image.open(io.BytesIO(image_data))
+        image.save(output_path, "PNG")
 
+    save_base64_to_png(data.file, 'jopa.png')
+    res = preprocessing_image('jopa.png')
 
-    # Возвращаем результат обработки изображения
     return JSONResponse(content={
         'productName': 'Овсянка, сэр',
         'validateResults': {
             'child3years': {
                 "validationTitle": "Для детей до 3х лет",
-                "isOk": True
+                "isOk": True,
+                'ocr_text': res,
+                # debug
             }
         }
     })
